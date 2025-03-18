1c1be0e8ccb3a98535fa2e41f47ce70d179001dc
Aditi <aditijb@cs.stanford.edu>
2025-03-18 11:04:40 -0700

    fixed scrolling bug

--- 4-fat-laptop/objs/pi-sd.d
+++ 4-fat-laptop/objs/pi-sd.d
@@ -1,7 +1,8 @@
 objs/pi-sd.o: pi-sd.c rpi.h demand.h gpio.h rpi-constants.h \
  your-prototypes.h pi-sd.h \
- /Users/suze/Desktop/cs140e-25win/libpi/libc/fast-hash32.h rpi.h \
- external-code/emmc.h /Users/suze/Desktop/cs140e-25win/libpi/libc/crc.h
+ /Users/aditi/Documents/cs140e-25win/libpi/libc/fast-hash32.h rpi.h \
+ external-code/emmc.h \
+ /Users/aditi/Documents/cs140e-25win/libpi/libc/crc.h
 
 rpi.h:
 
@@ -15,10 +16,10 @@ your-prototypes.h:
 
 pi-sd.h:
 
-/Users/suze/Desktop/cs140e-25win/libpi/libc/fast-hash32.h:
+/Users/aditi/Documents/cs140e-25win/libpi/libc/fast-hash32.h:
 
 rpi.h:
 
 external-code/emmc.h:
 
-/Users/suze/Desktop/cs140e-25win/libpi/libc/crc.h:
+/Users/aditi/Documents/cs140e-25win/libpi/libc/crc.h:
